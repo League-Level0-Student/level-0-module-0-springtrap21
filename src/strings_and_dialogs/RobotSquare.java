898e502c75a0400e14492dcebf4a024fd2d25d26
ilabstaff <ilabstaff@172.16.116.73>
2018-11-28 17:55:31 -0800

    robot spiral square

--- src/strings_and_dialogs/RobotSquare.java
+++ src/strings_and_dialogs/RobotSquare.java
@@ -12,22 +12,31 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot jeff= new Robot();
 
         // 3. Put the robot's pen down
-
+jeff.penDown();
 
         // 6. Make the robot move as fast as possible
 
-
+jeff.setSpeed(30000);
         // 5. Do everything below here 4 times
+for (int i = 0; i <10000000 ; i++) {
+	
+jeff.setRandomPenColor();
+
+jeff.setPenWidth(30);
+
+jeff.move(5*i);
+
 
 
         //         2. Move your robot 200 pixels
 
-
+jeff.move(5);
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
+jeff.turn(90);
 
     }
+}
 }
